9253bae16c3b935b25d72c867da19786aef9a494
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-06-27 20:38:33 -0500

    Timmy image errors and Exported Speakers

--- src/components/simplihacks/index.tsx
+++ src/components/simplihacks/index.tsx
@@ -3,5 +3,6 @@ import Hero from "./hero";
 import Sponsors from "./sponsors";
 import Staff from "./staff";
 import Winners from "./winners";
+import Speakers from "./speakers";
 
-export { Hero, Winners, FAQ, Sponsors, Staff };
+export { Hero, Winners, FAQ, Sponsors, Staff, Speakers };
