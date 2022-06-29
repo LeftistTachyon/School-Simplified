eaed8645e671175e513ecc81651b5d11765a892f
PranTanTheMan <46548127+PranTanTheMan@users.noreply.github.com>
2022-06-29 15:49:42 -0500

    Winners updated + nextjs sucks

--- src/components/simplihacks/winners.tsx
+++ src/components/simplihacks/winners.tsx
@@ -6,53 +6,67 @@ import type { ProjectWinner } from "types";
 
 const winners: ProjectWinner[] = [
 	{
-		names: "Ayush Agarwal, Krushay Bhavsar",
-		title: "Signslate",
-		prize: "1st Place Winner",
-		link: "https://devpost.com/software/signslate",
-		img: "/simplihacks/winners/signslate.png",
+		names: "TJ Bai",
+		title: "Pool",
+		prize: "1st Place Overall Winner",
+		link: "https://devpost.com/software/pool-8k9dvi",
+		img: "/simplihacks/winners/pool.png",
 	},
 	{
-		names: "Ananya Jajoo, Angelina Tsuboi, Bhavya Modi",
-		title: "Pocket CPR",
-		prize: "2nd Place Winner",
-		link: "https://devpost.com/software/pocket-cpr-ros5qh",
-		img: "/simplihacks/winners/pocketcpr.png",
+		names: "Jeremy Moon",
+		title: "EcoTheater",
+		prize: "2nd Place Overall Winner",
+		link: "https://devpost.com/software/ecotheater",
+		img: "/simplihacks/winners/EcoTheater.png",
 	},
 	{
-		names: "Sirini Karunadasa",
-		title: "International Gardener",
-		prize: "3rd Place",
-		link: "https://devpost.com/software/international-gardener",
-		img: "/simplihacks/winners/internationalgardener.png",
+		names: "Eric Li, Gautham Venkateshwaran ",
+		title: "The Trees",
+		prize: "3rd Place Overall Winner",
+		link: "https://devpost.com/software/the-trees",
+		img: "/simplihacks/winners/trees.png",
 	},
 	{
-		names: "Janine Jimenez, Tye Maxson, Ziyan Liu, Connie Guo",
-		title: "Study4Change",
-		prize: "Best Education",
-		link: "https://devpost.com/software/study4change",
-		img: "/simplihacks/winners/study4change.png",
+		names: "Aditya Kharote, Varun Koranne, Shreyash Dhamane, Noman Khan",
+		title: "WeChange",
+		prize: "1st College Overall Winner",
+		link: "https://devpost.com/software/wechange-l8vo1s",
+		img: "/simplihacks/winners/wechange.png",
 	},
 	{
-		names: "Asrith Sreeram",
-		title: "XTrack",
-		prize: "Best Social Good",
-		link: "https://devpost.com/software/xtrack",
-		img: "/simplihacks/winners/xtrack.png",
+		names: "Baron Liu, Jeremy Su, Emily Su, Avery Zeng",
+		title: "Nourpact",
+		prize: "2nd College Overall Winner",
+		link: "https://devpost.com/software/nourpact",
+		img: "/simplihacks/winners/nourpact.png",
 	},
 	{
-		names: "Hazim Arafa, Connie Z, Bonnie, Loid",
-		title: "Anon-Therapy",
-		prize: "Best Beginner",
-		link: "https://devpost.com/software/anon-therapy",
-		img: "/simplihacks/winners/anontherapy.png",
+		names: "Grace Gong, Azfar Mohamed, Aditya Bilawar, Duy Nguyen",
+		title: "Climate Connect",
+		prize: "3rd College Overall Winner",
+		link: "https://devpost.com/software/climate-connect-qwxgtl",
+		img: "/simplihacks/winners/climateconnect.png",
 	},
 	{
-		names: "Deorah ",
-		title: "SeniorSafety",
+		names: "Christopher Cao, Raymond Huang, Shourya Chhabra, Shreyas Nair",
+		title: "WikiEnv",
+		prize: "Best Social Cause",
+		link: "https://devpost.com/software/wikienv",
+		img: "/simplihacks/winners/WikiEnv.png",
+	},
+	{
+		names: "Laeek Ahmed, Shruti satheesh, Prashanthini Sundar, Partiv Aravind",
+		title: "Envirometics",
+		prize: "Best UI/UX",
+		link: "https://devpost.com/software/envirometics",
+		img: "/simplihacks/winners/Envirometics.png",
+	},
+	{
+		names: "Aaron Tang",
+		title: "Atmos",
 		prize: "Best Solo Project",
-		link: "https://devpost.com/software/seniorsafety",
-		img: "/simplihacks/winners/seniorsafety.png",
+		link: "https://devpost.com/software/atmos-hwzi09",
+		img: "/simplihacks/winners/Atmos.png",
 	},
 ];
 
@@ -65,7 +79,7 @@ export default function Winners(): JSX.Element {
 			<ContainerInside>
 				<VStack align="start">
 					<Heading fontSize="35px" my={7}>
-						Winners from June 2021
+						Winners from June 2022
 					</Heading>
 					<Flex w="100%" justify="center" flexWrap="wrap">
 						{winners.map((project, i: number) => {
