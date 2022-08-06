80f1098d7cc5ea2b7f897d766e389e40beffa79b
Gedeon Sainrival <gedeonsainrival1@gmail.com>
2022-08-06 11:41:34 -0400

    Incomplete testing

--- __tests__/pages/Home.test.tsx
+++ __tests__/pages/Home.test.tsx
@@ -1,8 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Home from "@pages/index";
 
-// NOTE: This test will intentionally fail since the name is not within any button element.
-
 describe("Home", () => {
 	it("renders the home page", () => {
 		render(<Home />);
